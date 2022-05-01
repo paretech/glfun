ca3b70989e0b81fd8405782990b259ae545388cb
Matt Pare <paretech@gmail.com>
2022-05-01 13:31:01 -0700

    Add basic scaffolding

---
+++ app.py
@@ -0,0 +1,60 @@
+# Copyright 2022 - Pare Technologies
+import logging
+import OpenGL.GL as gl
+# gl.ERROR_ON_COPY = True
+import os
+# os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = ''
+import pygame as pg
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+class App:
+    def __init__(self):
+        pg.init()
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,
+                                    pg.GL_CONTEXT_PROFILE_CORE)
+
+        pg.display.set_icon(pg.image.load('logo.png'))
+        pg.display.set_caption('App Title', 'App Icon Title')
+
+        display_flags = pg.OPENGL | pg.DOUBLEBUF
+        pg.display.set_mode((640, 480), display_flags)
+
+        self.clock = pg.time.Clock()
+
+        gl.glClearColor(0.1, 0.2, 0.2, 1)
+
+        self.main_event_loop()
+
+    def main_event_loop(self):
+        while True:
+            self.event_handler()
+            self.display()
+
+    def event_handler(self):
+        for event in pg.event.get():
+            log.debug(f'Detected event {event.type}')
+            if (event.type == pg.QUIT):
+                raise SystemExit
+
+    def display(self):
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+        pg.display.flip()
+        self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler(f'{__file__}.log'),
+            logging.StreamHandler()
+        ]
+    )
+
+    myApp = App()
---
+++ notes.md
@@ -0,0 +1,27 @@
+# Notes
+
+## Resources
+
+- [PyOpenGL Documentation](http://pyopengl.sourceforge.net/documentation/)
+- [Python & OpenGL for Scientific Visualization](https://www.labri.fr/perso/nrougier/python-opengl/)
+- [OpenGL with Python (youtube)](https://www.youtube.com/playlist?list=PLn3eTxaOtL2PDnEVNwOgZFm5xYPr4dUoR)
+- [NeHe Tutorials](https://nehe.gamedev.net/)
+- [Setup linting in VS Code](https://code.visualstudio.com/docs/python/linting)
+- [OpenGL Course - Create 3D and 2D Graphics With C++ (YouTube)](https://www.youtube.com/watch?v=45MIykWJ-C4)
+- [OpenGL with Python (YouTube Playlist)](https://www.youtube.com/playlist?list=PLn3eTxaOtL2PDnEVNwOgZFm5xYPr4dUoR)
+- [Associated GitHub for "OpenGL with Python" playlist](https://github.com/amengede/getIntoGameDev)
+
+
+## PyOpenGL Highlights
+
+The following bullets are derrived from reading [PyOpenGL for OpenGL Programmers](http://pyopengl.sourceforge.net/documentation/opengl_diffs.html).
+
+- Errors are raised as exceptions, rather than requiring the user to check return values and/or call glCheckError manually.
+- By default, PyOpenGL will log errors to the Python logging module. 
+- Supports a significant range of array-compatible data-types
+- Don't use lists, tuples, and numbers for performance critical operations.
+- You can set a flag to raise exception if there is excessive conversion of numpy arrays. See `OpenGL.ERROR_ON_COPY = True`. 
+- Since all the memory allocation in Python is automatic there is no need for glGetPointerv function.
+- PyOpenGL has support for most OpenGL extensions.
+- There are some differences in how Selection Buffers work
+
